a49d013df2ae182d5c1ec63d313db99c60bf29f3
Richard Sequera <121109089+RoyalB3@users.noreply.github.com>
2026-02-20 02:31:38 -0400

    Update mobileNav.tsx

--- components/ui/home/navbar/mobileNav.tsx
+++ components/ui/home/navbar/mobileNav.tsx
@@ -9,29 +9,21 @@ type Props = {
 }
 const MobileNav = ({closeNav, showNav}: Props) => {
 
-    const SideBarOpenClose = showNav ? "translate-x-0" : "transalate-x-[-100%]";
+    const SideBarOpenClose = showNav ? "translate-x-0" : "translate-x-[-100%]";
 
-  return (
-    <div>
-        {/* Overlay */}
-        <div className={`fixed ${SideBarOpenClose} inset-0 transform transition-all 
-        duration-500 z-1002 bg-black opacity-70 w-full h-screen`}></div>
-        {/* links */}
-        <div className={`text-white ${SideBarOpenClose} fixed justify-center flex flex-col h-full transform
-        transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-purple-700 space-y-6 z-1050`}>
-            {NavLinks.map((link,index) =>{
-                return (<Link key={index} href={link.href}>
-                    <p className='text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 
-                        border-white sm:text-[30px]'>
-                        {link.name}
-                    </p>
+  return ( 
+     <div>
+        <div className={`fixed ${SideBarOpenClose} inset-0 transform transition-all duration-500 z-1002 bg-black opacity-70 w-full h-screen`}></div>
+        <div className={`text-white ${SideBarOpenClose} fixed justify-center flex flex-col h-full transform transition-all duration-500
+        delay-300 w-[80%] sm:w-[60%] bg-purple-700 space-y-6 z-1050`}>
+            {NavLinks.map((link,index) => {
+                return <Link key={index} href={link.href}>
+                    <p className='text-white w-fit text-[20px] ml-12 border-p-[1.5px] pb-1 border-white sm:text-[30px]'>{link.name}</p>
                 </Link>
-                );
             })}
-            <X onClick={closeNav} className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6'/>
-{/*             <X onClick={closeNav} className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6'/>
- */}        </div>     
-    </div>
+            <XIcon onClick={closeNav} className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 cursor-pointer'></XIcon>
+        </div>
+    </div> 
   )
 }
 
